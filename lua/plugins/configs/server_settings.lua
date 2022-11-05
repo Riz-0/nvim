e4a31ca17adeeb6b5d263751360fc337d9f2eed6
Riz <55377396+Riz-0@users.noreply.github.com>
2022-11-05 15:55:15 +0800

    Add default handlers for lsp servers

--- lua/plugins/configs/server_settings.lua
+++ lua/plugins/configs/server_settings.lua
@@ -1,17 +1,28 @@
--- LSP Server Configurations
+-- Configure lsp servers
 local lspconfig = require("lspconfig")
 local M = require("plugins.configs.lsp")
 
-lspconfig.sumneko_lua.setup({
-  on_attach = M.on_attach,
-  flags = M.lsp_flags,
-  capabilities = M.capabilities,
-  -- Server settings
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim", }
+require("mason-lspconfig").setup_handlers({
+  function (server_name)  -- Default handler
+    lspconfig[server_name].setup({
+      on_attach = M.on_attach,
+      flags = M.lsp_flags,
+      capabilities = M.capabilities,
+    })
+  end,
+  ["sumneko_lua"] = function ()
+    lspconfig.sumneko_lua.setup({
+      on_attach = M.on_attach,
+      flags = M.lsp_flags,
+      capabilities = M.capabilities,
+      -- Server settings
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim", }
+          }
+        }
       }
-    }
-  }
+    })
+  end
 })
