beddf5b541d589ce38a37674c8920207681064c9
Riz <55377396+Riz-0@users.noreply.github.com>
2022-11-06 17:24:30 +0800

    Configure navigation keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,3 +14,16 @@ map("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
 -- Indentation
 map("v", "<", "<gv", opts)
 map("v", ">", ">gv", opts)
+
+-- Window Navigation
+map("n", "<S-h>", "<C-w>h", opts)
+map("n", "<S-j>", "<C-w>j", opts)
+map("n", "<S-k>", "<C-w>k", opts)
+map("n", "<S-l>", "<C-w>l", opts)
+
+-- Buffer Navigation
+map("n", "<A-l>", ":bn<CR>", opts)
+map("n", "<A-h>", ":bp<CR>", opts)
+
+-- Delete Buffer
+map("n", "<C-w>", ":bd<CR>", opts)
